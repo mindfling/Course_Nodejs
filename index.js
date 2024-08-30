5a569bec7ec57b589e9a00c1558c527eb8258200
Nepochtov Dmitry <dimendroider@gmail.com>
2024-08-31 00:32:57 +0300

    feat get age with Luxon

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { DateTime, Interval, Duration } from "luxon";
+
 // на вход принимает объект такого вида
 /*
 {
@@ -6,9 +8,6 @@
   purpose: 'карьерный рост'
 }
 */
-
-import { DateTime, Interval } from "luxon";
-
 // возвращает
 /*
 {
@@ -33,34 +32,23 @@ const getId = (len = 10) => {
   return result;
 };
 
-const calculateAge = (dateBirth) => {
-  console.log('dateBirth: ', dateBirth);
-  // парсим строку даты
-const [date, month, year] = dateBirth.split(".").map(n => parseInt(n))
-  // const date = parseInt(dateBirth.split(".")[0]);
-  // const month = parseInt(dateBirth.split(".")[1]);
-  // const year = parseInt(dateBirth.split(".")[2]);
-  // проверить
-  
-  const birth = DateTime.local(year, month, date)
-  console.log('year, month, date: ', year, month, date);
-  console.log('\n\nbirth: ', birth);
-  const curr = DateTime.now();
-  console.log('curr: ', curr);
 
-  const age = Interval.fromDateTimes(birth, curr).toDuration('years').years;
-  console.log('age: ', age);
+// * вычисляем возраст полных лет с помощью Luxon
+const calculateAge = (dateBirth) => {
+  // парсим строку даты
+  const [date, month, year] = dateBirth.split(".").map((n) => parseInt(n));
+  // проверить
+  const birth = DateTime.local(year, month, date); // ? проверить дата рождения
+  const curr = DateTime.now(); // ? проверить текущая дата
+  const age = Interval.fromDateTimes(birth, curr).toDuration("years").years;
 
   return Math.trunc(age);
-
-  // const birthTime = new Date(`${month}-${date}-${year}`).getTime();
-  // const currentTime = new Date().getTime();
-  // вычисляем возраст из разницы милисекунд
-  // const age = Math.trunc((currentTime - birthTime) / 1000 / 3600 / 24 / 365);
 };
+
 
 const capitalize = (str) =>
   `${str.charAt(0).toUpperCase()}${str.substring(1).toLowerCase()}`;
+
 
 const createWorker = (person) => {
   const worker = {
@@ -70,10 +58,10 @@ const createWorker = (person) => {
     dateBirth: person.dateBirth,
     age: calculateAge(person.dateBirth),
     purpose: capitalize(person.purpose),
-    
   };
   return worker;
 };
+
 
 const personOne = {
   name: "иван иванов",
@@ -81,17 +69,16 @@ const personOne = {
   purpose: "карьерный рост",
 };
 
-// const personTwo = {
-//   name: 'шелдон ли купер',
-//   dateBirth: '26.02.1980',
-//   purpose: 'получаю Нобелевскую премию',
-// };
+const personTwo = {
+  name: 'шелдон ли купер',
+  dateBirth: '26.02.1980',
+  purpose: 'получаю Нобелевскую премию',
+};
 
 const workerOne = createWorker(personOne);
 console.log("\npersonOne: ", personOne);
 console.log("workerOne: ", workerOne);
 
-// const workerTwo = createWorker(personTwo);
-// console.log('\npersonTwo: ', personTwo);
-// console.log('workerTwo: ', workerTwo);
-
+const workerTwo = createWorker(personTwo);
+console.log('\npersonTwo: ', personTwo);
+console.log('workerTwo: ', workerTwo);
