a0919af9a730f7c82b733ca9736bcced3fc90b7a
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2024-09-07 20:26:48 +0300

    feat tick every 1sec

--- Lesson4task1/index.js
+++ Lesson4task1/index.js
@@ -1,1 +1,27 @@
+import { EventEmitter } from 'node:events';
+
 console.log('Hello Event Emmiter');
+
+
+// event emmiter
+
+class MyEventEmitter extends EventEmitter {
+  
+}
+
+// instance of my event emmiter
+
+const myEventEmmiter = new MyEventEmitter();
+
+myEventEmmiter.addListener('tick', () => {
+  console.log('listener tick');
+});
+
+
+
+// * test
+
+// tick every 1 second
+setInterval(() => {
+  myEventEmmiter.emit('tick');
+}, 1000)
