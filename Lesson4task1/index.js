d640e07a59fa0aee98da0fdb5d2f09f44aa786f9
Nepochtov Dmitry <dimendroider@gmail.com>
2024-09-08 02:20:56 +0300

    feat with console.log

--- Lesson4task1/index.js
+++ Lesson4task1/index.js
@@ -5,24 +5,32 @@ console.log('Hello Event Emmiter');
 
 // settings
 var count = 0;
-const timeout = 500;
+const timeout = 100;
 const tick = 'tick';
 const MAX_TICK = 10;
 
+const begin = Date.now(); // начало отсчета
+const countTime = () => (Date.now() - begin);
 
 // my event emmiter
 class EE extends EventEmitter {}
 
 const ee = new EE();
 
-ee.once(tick, () => {
+
+ee.on(tick, () => {
   count++;  
-  console.log(` tick - ${count}`);
+  console.log(` Tick - ${count} <- ${countTime()}`);
   if (count >= MAX_TICK) {
     ee.removeAllListeners(tick);
     clearInterval(tickTimer);
+    ee.emit('end', countTime())
   }
 });
+
+ee.on('end', (m = '') => {
+  console.log(`The end of Day: ${m}`);
+})
 
 
 // * test
