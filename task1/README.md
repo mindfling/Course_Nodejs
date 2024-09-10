4639a0672e75b532cb05cd7da34172a6c502448c
Nepochtov Dmitry <dimendroider@gmail.com>
2024-09-10 11:24:53 +0300

    task one procedures

---
+++ task1/README.md
@@ -0,0 +1,25 @@
+# Course_Nodejs
+
+## lesson04 task1
+
+Ans: <https://github.com/mindfling/Course_Nodejs/tree/lesson04>
+
+> Создайте таймер, который будет генерировать событие `tick` и выводить сообщение в консоль.
+
+```js
+  node index
+
+  Tick - 1
+  Tick - 2
+  Tick - 3
+  Tick - 4
+    ...
+```
+
+> [!NOTE]
+>
+> Используйте EventEmmiter
+
+Author: *Дмитрий Дим Непочтов*
+
+Решение в процедурном стиле: <https://github.com/mindfling/Course_Nodejs/tree/lesson04/task1>
--- task1/index.js
+++ task1/index.js
@@ -1,21 +1,22 @@
 import { EventEmitter } from "node:events";
 
-// * just simple dummy procedure style
+// * just simple dummy procedure paradigme
 class EE extends EventEmitter {
 }
 
 const ee = new EE();
 
-
+// settings
+const timeout = 1000;
+const MAX_TICK = 10;
 let count = 0;
 
-const tickHandle = (x) => {
-  console.log(` Tick - ${++count} <= ${x}`);
-
-  if (count > 10) {
+const tickHandle = () => {
+  ++count;
+  console.log(` Tick - ${count}`);
+  if (count > MAX_TICK) {
     clearInterval(tickTimer);
     ee.removeAllListeners('tick');
-    console.log('timer stoped');
   }
 }
 
@@ -25,10 +26,10 @@ ee.on('tick', tickHandle);
 // * test ticks in 1000ms
 const tickTimer = setInterval(() => {
   ee.emit('tick', count)
-}, 1000);
+}, timeout);
 
 
 // * emegency stop timer
 setTimeout(() => {
   clearInterval(tickTimer);
-}, 25000);
+}, 20000);
