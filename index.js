5c1d8ea5d47d4061511539edc13bb0208c45a0aa
Nepochtov Dmitry <dimendroider@gmail.com>
2024-09-19 07:29:28 +0300

    copying files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello NPM');
+import { copyFile } from "./modules/copyFile.js";
+
+
+const App = async () => {
+  console.log('Hello NPM');
+
+  await copyFile('./files/tex.txt', './files/copy.txt', err => {
+    console.error('Where is error');
+  })
+}
+
+
+// * test *
+App();
