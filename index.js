ed289072bdf2ad61b191363c3088e5f276f4d053
Nepochtov Dmitry <dimendroider@gmail.com>
2024-09-24 08:53:15 +0300

    feat read and copy Sync

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-import { copyFile } from "./modules/copyFileCallback.js";
+// import { copyFile } from "./modules/copyFileCallback.js";
+import { copyFileSync, copyTextFileSync, readDir } from './modules/copyFileSync.js';
 
-const handleCopyError = (err) => {
+const handleCopyError = err => {
   console.log('\nhello handle');
-  console.error('МОЙ ОБРАБОТЧИК ОШИБОК:')
-  console.log('handleCopyError', err.errno, err.code, err.syscall, 'at PATH', err.path);
+  console.error('МОЙ ОБРАБОТЧИК ОШИБОК:');
+  console.log(
+    'handleCopyError',
+    err.errno,
+    err.code,
+    err.syscall,
+    'at PATH',
+    err.path,
+  );
   // throw new Error('ОБРАБОТЧИК ОШИБКИ ПРИ КОПИРОВАНИИ')
   console.log();
-}
+};
 
-
-const App =  () => {
+const App = () => {
   console.log('hello');
 
+  const fromPath = './files/tex.txt';
+  const toPath = './files/tex.bak';
+  
   try {
     console.log('\nbefore await');
-    copyFile('./1files/tex.txt', './files/copy.txt', handleCopyError);
+    readDir('files');
+    copyTextFileSync(fromPath, toPath);
     console.log('after await\n');
-
   } catch (error) {
     console.log('error: ', error);
-    console.log('11 there occure error, while coping file', error);
   }
 }
 
--- modules/copyFileSync.js
+++ modules/copyFileSync.js
@@ -1,18 +1,67 @@
-import fs from 'node:fs';
+import fs, { readdirSync } from 'node:fs';
 
 // * !! синхронное копирорвание !!
 
-const copyFileSync = (sourse, target) => {
+export const copyFileSync = (sourse, target) => {
+  // todo проверка корректроно адреса
 
   try {
-    const result = fs.readFileSync(sourse, 'utf8');
+    // читаем файл
+    const result = fs.readFileSync(sourse);
     console.log('прочитали файл');
+
+    // записываем файл
     fs.writeFileSync(target, result);
     console.log('записли файл');
 
   } catch (error) {
-    console.log("Ошибка при копировани");    
+    console.error("18 copyFileSync Ошибка при копировани at", error.path);
+    error.msg = 'Ошибка при копировани at copyFileSync module';
+    throw error;
   }
 
-  console.log('Скопировано без ошибок');
+  console.log('\nСкопировано без ошибок\n');
 }
+
+
+export const copyTextFileSync = (sourse, target) => {
+  // todo проверка корректроно адреса
+
+  try {
+    // читаем файл
+    const result = fs.readFileSync(sourse, 'utf8');
+    console.log('прочитали файл');
+
+    // записываем файл
+    fs.writeFileSync(target, result);
+    console.log('записли файл');
+
+  } catch (error) {
+    console.error("40 copyFileSync Ошибка при копировани at", error.path);
+    error.msg = 'Ошибка при копировани at copyTextFileSync module';
+    throw error;
+  }
+
+  console.log('\nСкопировано без ошибок\n');
+}
+
+
+export const readDir = (path) => {
+  console.log();
+  console.log(`path: ${path}`);
+  const result = fs.readdirSync(path);
+  console.log('result: ', result);
+  
+  result.forEach(item => {
+    const itemPath = `${path}/${item}`;
+    const stat = fs.statSync(itemPath);
+    console.log(item, stat.isFile() ? `-f size=${stat.size}` : '--', stat.isDirectory() ? 'd' : '-');
+
+    if (stat.isDirectory()) {
+      readDir(itemPath); // into recursive
+    }
+  })
+
+  console.log();
+}
+
