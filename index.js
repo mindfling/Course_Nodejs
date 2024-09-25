b5fa7747e225dfb4fcf6a50ed39fadab6fa66a2d
Nepochtov Dmitry <dimendroider@gmail.com>
2024-09-26 01:19:17 +0300

    feat draft read write cb

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-import { Logger } from "./modules/Logger.js";
+import { Logger } from './modules/Logger.js';
 
 console.log('Hello index logger');
 
 // todo test Logger see task2
 const logger = new Logger('log.txt', 1024);
+console.log('logger: ', logger);
 
-logger.on('messageLogged', message => {
-  console.log('Записано сообщение:', message);
-});
+// logger.on('messageLogged', message => {
+//   console.log('Записано сообщение:', message);
+// });
 
-logger.log('Первое сообщение');
+logger.log('Первое сообщение ');
+console.log('logger: ', logger);
+
 logger.log('Второе сообщение');
-
+console.log('logger: ', logger);
--- modules/Logger.js
+++ modules/Logger.js
@@ -1,43 +1,69 @@
-import { EventEmitter } from "node:events";
-import fs from "node:fs";
+import { EventEmitter } from 'node:events';
+import fs from 'node:fs';
 
 // todo Logger class
 console.log('hello logger');
 
-
 export class Logger extends EventEmitter {
-
   constructor(filename, maxSize) {
     super();
+
     this._filename = filename; // имя файла лога
-    this._maxSize = maxSize;  //  максимальный размер файла лога
-    this._logQueue = [];  // масс очередь сообщений
-    this._writing = false; // в данный момент isWriting
+    this._maxSize = maxSize; //  максимальный размер файла лога
 
-    this.on('messageLogged', () => {
-      // todo
-      this.log()
-      this.writingLog()
-      this.rotateLog()
-      this.checkFileSize()
-      fs.truncate(this._filename)
-    })
+    this._logQueue = []; // масс очередь сообщений
+    this._writing = false; // в данный момент not isWriting
+
+    this._currentFileSize = 0;
   }
 
-  log(message) { // отправляет сообщение
-    this._logQueue.shift(message); // добавляем сообщение в очередь в начало
-    writingLog(); 
+  log(message) {
+    // отправляет сообщение
+    console.log('log message: ', message);
+    this._logQueue.push(message); // добавляем сообщение в очередь сверху
+    // writingLog();
     this._writing = true;
-    // todo
-    this.emit('messageLogged');
-    this.checkFileSize()
+    // this.emit('messageLogged');
+    // this.checkFileSize()
+
+    // читаем файл и добавляем
+    fs.readFile(this._filename, { encoding: 'utf-8' }, (readerr, result) => {
+      //читаем
+      if (readerr) {
+        console.log(`Ошибка чтения файла read file error ${this._filename}`);
+        return;
+      }
+      console.log('файл прочитан из', this._filename);
+      console.log('result: ', result);
+
+      // дозаписываем в файл
+      fs.writeFile(this._filename, result + '\n' + message, err => {
+        // перезапись всего файла
+        if (err) {
+          console.log(`Ошибка записи в файл: ${this._filename}`);
+
+        } else {
+          console.log(`Сообщение записано в файл: ${this._filename}`);
+        }
+
+        fs.stat(this._filename, ( err, stats ) => {
+          if (err) console.log(`Ошибка чтени stat файла`);
+
+          console.log('stat: ', stats.size, 'Байт');
+          this._currentFileSize = stats.size;
+        });
+
+
+      });
+    });
   }
 
+  /*
   writingLog() { // запис сообщение в файл лог
     // todo
-    const nextMessage = this._logQueue.pull(); // достаем сообщение из очереди
+    const nextMessage = this._logQueue.shift(); // достаем сообщение из очереди снизу
 
-    if (this._logQueue.length > 0) {
+    if (this._logQueue.length > 0) { // если в очереди еще есть задания
       this.writingLog();
     }
   }
@@ -60,6 +86,5 @@ export class Logger extends EventEmitter {
   rotateLog() {
     // todo
   }
+  */
 }
-
-
