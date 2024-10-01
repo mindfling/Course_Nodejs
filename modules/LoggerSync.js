beff5a7ecdc4a2e42ca8a175bba7fb0fa90a0db9
Nepochtov Dmitry <dimendroider@gmail.com>
2024-10-02 01:57:35 +0300

    log truncake

--- modules/LoggerSync.js
+++ modules/LoggerSync.js
@@ -9,9 +9,9 @@ export class Logger extends EventEmitter {
     super();
     this._filename = filename; // имя файла лога
     this._maxSize = maxSize; //  максимальный размер файла лога
-    this._logQueue = ['Привет', 'как дела', 'Все ок']; // масс очередь сообщений
+    this._logQueue = []; // масс очередь сообщений
     this._writing = false; // в данный момент not isWriting
-    this._currentFileSize = 0;
+    // this._currentFileSize = 0;
   }
 
 
@@ -31,21 +31,52 @@ export class Logger extends EventEmitter {
     console.log('Запись в лог');
 
     while (this._logQueue.length > 0) {
-      const message = this._logQueue.shift();
+      const message = this._logQueue.shift(); // забираем из начала очереди
 
       console.log('сначала прочитаем из файла', this._filename);
-      let data = fs.readFileSync(this._filename, {encoding: 'utf8'});
+      let data = '';
+      
+      try {
+        data = fs.readFileSync(this._filename, {encoding: 'utf8'});
+      } catch (error) {
+        console.log('файл не существует');
+      }
 
-      console.log('запишем в файл в начало', this._filename);
+      // запишем в файл в начало
       fs.writeFileSync(this._filename, `${message}\n${data}`);
 
-      this.emit('messageLogged', message);
+      this.emit('messageLogged', message); // генерируем событие сообщения
     }
 
     if (this._logQueue <= 0) {
       this._writing = false;
       console.log("сообщений нет");
     }
+
+    this.checkFileSize();
+  }
+
+
+  getFileSize() {
+    const stats = fs.statSync(this._filename);
+    const fileSize = stats.size; // размер в байтах
+    return fileSize;
+  }
+
+
+  checkFileSize() {
+    const currentFileSize = this.getFileSize();
+    console.log('currentFileSize: ', currentFileSize, 'Bytes');
+
+    if (currentFileSize > 1124) {
+      fs.truncateSync(this._filename, 1024);
+      console.log('файл транкейтер');
+    }
+  }
+
+
+  rotateLog() {
+    // create log.bak
   }
 
 }
