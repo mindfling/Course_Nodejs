5997ccd0cfbd4faff7eeb7514ba4b00a14514680
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-04 18:34:05 +0300

    try textToBuff buffToText

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ initial node template for Lesson06:
   - [Lesson06 Task1](https://github.com/mindfling/Course_Nodejs/tree/lesson06task1)
   - [Lesson06 Task2](https://github.com/mindfling/Course_Nodejs/tree/lesson06task2)
   - [Lesson06 Task3](https://github.com/mindfling/Course_Nodejs/tree/lesson06task3)
+
+### Задание #1
+
+Создайте функции для кодирования и декодирования текста из различных кодировок в буфер и наоборот.
+Например, переведите текст из `UTF-8` в `Base64` и обратно `utf8`
+
--- index.js
+++ index.js
@@ -2,4 +2,23 @@ import { log, error } from 'node:console';
 import { read } from './modules/read.js';
 import { write } from './modules/write.js';
 
-console.log('Hello NPM');
+console.log('Hello Buffer');
+
+const textToBuffer = (text, encode) => {
+  return Buffer.from(text, encode);
+}
+
+const bufferToText = (buff, encode) => {
+  return Buffer.from(buff, encode).toString();
+}
+
+
+// todo Проверка
+const text = 'Привет Мир!';
+log({text});
+
+const utf8Buff = textToBuffer(text, 'utf8');
+log({utf8Buff});
+
+const decodeText = bufferToText(utf8Buff, 'utf8');
+log({decodeText});
