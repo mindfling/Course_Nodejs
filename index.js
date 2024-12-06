5376db20ae3bd8cf171b5326cf4f9a0299a0f2bc
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-06 16:46:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,163 +5,103 @@ import { log } from 'node:console';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: '\x1b[32mYour answer: \x1b[0m',
+  prompt: '\x1b[32m#: \x1b[0m',
+  // prompt: '\x1b[32mYour answer: \x1b[0m',
 });
 
+// GLOBALS
 let test = [];
 let count = 0;
 let current = null;
 
+const init = async () => {
+  const buff = await read('./game/question.json');
+  // console.log('\nbuff buff:', typeof buff, '=', buff);
+  const text = buff.toString('utf8');
+  //? Вопрос //test = buff.toJSON() //почему не работает
+  // console.log('\ntext:', typeof text, '=', text);
+  test = JSON.parse(text);
+  // console.log('\ntest:', typeof test, '=', test);
+};
 
 const nextQuestion = () => {
-  current = test[count];
-  log('Текущий вопрос:', current.question)
-  log('варианты ответа:')
-  current.options.forEach((option, j) => {
-    log(`${j+1}: ${option}`)
-  });
-  count++;
-  if (test[count >= test.length]) {
-    log('that is the end')
+  count++; // next count
+  if (count >= test.length) {
+    log('Это был последний ворпос');
     rl.close();
   }
-}
-
-const init = () => {
-  // test = await read('./game/question.json');
-  // console.log('test: ', test);
-
-  /*
-  test = [
-    {
-      "question": "Что такое CommonJS?",
-      "options": [
-        "Модульная система для разработки веб-приложений на Node.js",
-        "Стандарт для организации модулей в JavaScript",
-        "Библиотека для работы с базой данных в Node.js"
-      ],
-      "correctIndex": 1
-    },
-    {
-      "question": "Какие встроенные модули позволяют работать с файловой системой в Node.js?",
-      "options": [
-        "http и https",
-        "crypto и zlib",
-        "http и https",
-        "crypto и zlib",
-        "http и https",
-        "crypto и zlib",
-        "fs и path"
-      ],
-      "correctIndex": 2
-    },
-    {
-      "question": "Что такое EventEmitter в Node.js?",
-      "options": [
-        "Модуль для работы с базой данных",
-        "Фреймворк для разработки веб-серверов",
-        "Класс, который позволяет генерировать и слушать события"
-      ],
-      "correctIndex": 2
-    },
-    {
-      "question": "Какие встроенные модули позволяют работать с файловой системой в Node.js?",
-      "options": [
-        "http и https",
-        "crypto и zlib",
-        "fs и path"
-      ],
-      "correctIndex": 2
-    },
-    {
-      "question": "Что такое EventEmitter в Node.js?",
-      "options": [
-        "Модуль для работы с базой данных",
-        "Фреймворк для разработки веб-серверов",
-        "Класс, который позволяет генерировать и слушать события"
-      ],
-      "correctIndex": 2
-    },
-    {
-      "question": "Какие встроенные модули позволяют работать с файловой системой в Node.js?",
-      "options": [
-        "http и https",
-        "crypto и zlib",
-        "fs и path"
-      ],
-      "correctIndex": 2
-    },
-    {
-      "question": "Что такое EventEmitter в Node.js?",
-      "options": [
-        "Модуль для работы с базой данных",
-        "Фреймворк для разработки веб-серверов",
-        "Класс, который позволяет генерировать и слушать события"
-      ],
-      "correctIndex": 2
-    },
-  ]
-  */
-
-  log('\x1b[35mHello, lets play ))\x1b[0m');
+  console.log('Вопрос', count, ':');
+  current = test[count];
+  console.log('Текущий вопрос:', current.question);
+  console.log('Варианты ответов:');
+  current.options.forEach((option, j) => {
+    log(`${j + 1}: ${option}`);
+  });
+  console.log('Ваш ответ: ');
+  rl.setPrompt('Ваш вариант ответа: ')
   rl.prompt();
 };
 
+const checkAnswer = userAnswer => {
+if (userAnswer != 1 || userAnswer != 2 || userAnswer != 3 ) {
+
+} else if (userAnswer === '0') {
+    log('Вы ответели 0 ---> команда к завершению игры');
+  } 
+};
 
 rl.on('line', ans => {
-  log(`Ваш ответ "${ans}"`);
-  log(count);
+  log(`Вы ответели "${ans}"\n\n`);
 
-  //todo
-  if (count > 5) {
-    rl.close();
-    return;
+  checkAnswer(ans); //todo
 
-  } else if (ans === 'exit' || ans === 'quit' || ans === '0') {
-    log('\x1b[1;35mGood bye and Good night')
+  if (ans === 'exit' || ans === '0') {
+    log('\n\x1b[1;35mЗакрываем приложение');
     rl.close();
     // ---> process.exit();
     return;
 
-  } else if (ans === 'test') {
-    log(test);
-    rl.emit('line')
-
-  } else if (ans === 'time') {
-    log('now is the time:');
-    const time = new Date();
-    log(`time is:
-      год ${time.getFullYear()} месяц ${time.getMonth()} число ${time.getDate()} день ${time.getDay()}
-      `)
-    // rl.emit('line')
-    nextQuestion();
-
-  } else {
-    // продолжаем вопросы
-    log('\nСкажи что-нибудь еще...')
-    nextQuestion();
-    log('промпт')
-    rl.prompt();
+    // } else if (count >= test.length) {
+    //   log('Это был последний ворпос');
+    //   rl.close();
+    //   return;
   }
+
+  nextQuestion();
+
+  // } else if (ans === 'test') {
+  //   log(test);
+  //   rl.emit('line')
+
+  // } else if (ans === 'time') {
+  //   log('now is the time:');
+  //   const time = new Date();
+  //   log(`time is:
+  //     год ${time.getFullYear()} месяц ${time.getMonth()} число ${time.getDate()} день ${time.getDay()}
+  //     `)
+  //   // rl.emit('line')
+  //   nextQuestion();
+  // } else {
+  //   // продолжаем вопросы
+  //   log('\nВаш вариант ответа')
+  //   nextQuestion();
+  //   rl.prompt();
+  // }
 });
 
 rl.on('close', () => {
-  console.error('Good Bye! App closed');
+  log('\x1b[1;35mGood bye and Good night');
+  // App closed
   process.exit();
 });
 
-
 const app = async () => {
-  // init();
+  await init();
+  log('Количество вопросов в Квизе:', test.length);
 
-  const buff = await read('./game/question.json')
-  console.log('\nbuff buff:', typeof buff, '=', buff);
-
-  const text = buff.toString('utf8');
-  console.log('\ntext:', typeof text, '=', text);
-
-  test = JSON.parse(text);
-  console.log('\ntest:', typeof test, '=', test);
-}
+  log('\x1b[35mHello, Давайте поиграем\x1b[0m\n');
+  // rl.prompt();
+  nextQuestion();
+};
 
 app();
