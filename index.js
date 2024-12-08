202598b23d3de4e7dcd9b883195ef9aa385c2da2
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-08 23:52:09 +0300

    feat loadQuiz begin gameCycle

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { error } from 'node:console';
 import { readFile, writeFile } from 'node:fs/promises';
-import path, {dirname, join, basename, extname, resolve} from 'node:path';
+import path, { dirname, join, basename, extname, resolve } from 'node:path';
 import URL from 'node:url';
-
 
 const __filename = URL.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,54 +11,51 @@ console.log();
 
 const file = join(__dirname, 'game/question.json');
 console.log('file: ', file);
-const resultDir = dirname(file)
-console.log('resultDir: ', resultDir);
-
+const resultDir = dirname(file);
 const newFile = join(resultDir, basename(file, extname(file)) + '.txt');
 console.log('newFile: ', newFile);
 
+const loadQuiz = async path => {
+  let quiz = []; //todo
+  await readFile(path)
+    .then(buff => buff.toString('utf8'))
+    .then(text => JSON.parse(text))
+    .then(json => {
+      console.log(`файл "${path}" успешно прочитан и распарсин`);
+      quiz = json;
+    })
+    .catch(err => {
+      console.error(`Ошибка чтения файла "${path}" : ${err.message}`);
+    });
+  return quiz;
+};
 
-let quiz = [];
-let current = {};
-let count = 0;
-let rightAns = 0;
-
-
-const loadQuiz = async (path) => {
-  try {
-    const buff = await readFile(path);
-    const text = buff.toString('utf8');
-    const json = JSON.parse(text);
-    return json;
-  } catch(err) {
-    console.error(`Ошибка чтения файла "${path}" : ${err.message}`);
-  }
-}
+const gameCycle = quiz => {
+  console.log();
+  quiz.forEach((qu, i) =>
+    error(
+      `quest ${i}: "${qu.question}" ans=${qu.correctIndex} > right: "${qu.options[qu.correctIndex]}"`,
+    ),
+  );
+  console.log();
+};
 
 const app = async () => {
+  let current = {};
+  let count = 0;
+  let rightAns = 0;
+
   // hello();
   // init();
+
   const quiz = await loadQuiz(file);
-  console.log('\n quiz: ', typeof quiz);
-
-  const list = quiz.map((qu, i) => `quest ${i}: "${qu.question}" ans=${qu.correctIndex} > right: "${qu.options[qu.correctIndex]}"`)
-  console.log('list: ', list);
-
-  let listTextQuick = '';
-  list.forEach(value => {
-    listTextQuick += `${value}\n`;
-  });
-
-  
-  try {
-    writeFile(newFile, listTextQuick);
-    console.log(`Файл "${newFile}" записан`);
-  } catch(err) {
-    console.error(`Ошибка записи в файл "${newFile}" : ${err.message}`);
+  // console.log('quiz app: ', quiz);
+  if (!quiz) {
+    error('Невозможно загрузить вопросы квиза');
+    return;
   }
 
-  // gameCycle();
-}
-
+  gameCycle(quiz);
+};
 
 app();
