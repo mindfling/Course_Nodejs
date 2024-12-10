606077d5607d9918c48580fdaf62fe55ba5d94f3
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-10 20:28:51 +0300

    feat cli hello

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ console.log('Hello CLI');
 
 
 const app = () => {
-  
   console.log('hello');
+  
+  const args = process.argv;
+  console.log('args: ', args);
+
+  const arg = process.argv.slice(2)
+  console.log('arg: ', arg, arg[0]);
+  console.log('Привет ', arg[0]);
 }
+
+
+app();
