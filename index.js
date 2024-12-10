2203b133b7eacbd4e176315907025dc95d9c4783
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-10 21:00:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-console.log('Hello CLI');
+import { argsParse } from './util/argsParse.js';
 
+console.log('Hello CLI');
 
 const app = () => {
   console.log('hello');
-  
+
   const args = process.argv;
   console.log('args: ', args);
 
-  const arg = process.argv.slice(2)
+  const arg = process.argv.slice(2);
   console.log('arg: ', arg, arg[0]);
   console.log('Привет ', arg[0]);
-}
-
+};
 
 app();
