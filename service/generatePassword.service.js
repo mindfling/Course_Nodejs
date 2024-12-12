04692bfe2183aa6941d64b9fd1ea6e974d6555cf
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-12 06:24:14 +0300

    Create generatePassword.service.js

---
+++ service/generatePassword.service.js
@@ -0,0 +1,22 @@
+import { shuffle } from "../util/shuffle.util.js";
+
+export const generatePassword = option => {
+  log({option});
+  let charset = 'abcdefghijklmnopqrstuvwxyz';
+
+  if (option.uppercase) {
+    charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  if (option.number) {
+    charset += '0123456789';
+  }
+
+  if (option.special) {
+    charset += '!@#$%^&*(){}[]_-+=';
+  }
+
+  const result = shuffle(charset.split('')).join('');
+
+  return result.substring(0, option.length);
+};
