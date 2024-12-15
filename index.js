eb5131c297831e923f73a0e03f3a72af2c9f11b0
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-15 03:32:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,112 +1,166 @@
 #!/usr/bin/env node
-
 import { argsParse } from './util/argsParse.js';
-import { log } from 'node:console';
-
-import { readJsonData, readTasks } from './modules/read.js';
-import { writeTasks } from './modules/write.js';
+import { log, time } from 'node:console';
+import { readJsonData } from './modules/read.js';
+import { writeJsonData } from './modules/write.js';
 
 import { dirname, join, basename, extname } from 'node:path';
 import URL from 'node:url';
-
+import chalk from 'chalk';
 
 const __filename = URL.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const todoPath = join(__dirname, 'todolist.json');
+const __dirname = dirname(__filename);
+const todoPath = join(__dirname, 'todolist.json'); // путь к файлу БД
+console.log('todoPath: ', todoPath);
+
+
+
+
 
 
 const app = async () => {
   const args = process.argv;
+  console.log('args: ', args);
 
-  // список доступных команд
-  const words = [
-    'add',
-    'list',
-    'update',
-    'get',
-    'delete',
-    'help'
-  ];
+  // список доступных команд array
+  const words = ['add', 'list', 'update', 'get', 'delete', 'status', 'help'];
 
-  const options = argsParse(args, words); //?
-  let taskList = [];
-  taskList = await readJsonData(todoPath);
+  // список слов комманд in object
+  const commands = {
+    add: {
+      title: '',
+      status: 'В работе',
+    },
+    list: false,
+    update: {
+      title: '',
+      id: NaN,
+    },
+    get: {
+      id: NaN,
+    },
+    delete: {
+      id: NaN,
+    },
+    help: false,
+  };
 
-  const newTask = {
-    // id: 1,
-    title: 'Помыть слона',
-    status: 'Выполнено',
+  // func init();
+  const options = argsParse(args, words, commands); //?
+  console.log('РАСПАРСЕРЕННЫЕ options: ', options);
+
+  log('\ntodoPath: ', todoPath);
+  // const taskList = await readJsonData(todoPath);
+
+
+  const printTaskList = list => {
+    // list = readJsonData(todoPath); // здесь читаем todolist.json
+    log();
+    log(`Список задач:`);
+    list.forEach((task, index) => {
+      log(`\x1b[33m${index + 1}. \x1b[0m[${task.status}] ${task.title}`);
+    });
+  };
+
+
+  // todo list
+  // list: вывести список всех задач.
+  if (options.list) {
+    const taskList = await readJsonData(todoPath);
+    printTaskList(taskList);
   }
 
 
   // todo add
   // add <"task":string>: добавить новую задачу.
   if (options.add) {
-    // taskList = readTasks() //
-    const id = taskList.push({
-      // id: 1, // ! id это просто номер списка
-      title: options.add,
-      status: 'В работе',
-    })
-    // writeTasks(taskList)
-    log('Задача добавлена с идентефикатором', id)
+    if (options.text) {
+      const taskList = await readJsonData(todoPath);
+      const id = taskList.push({
+        title: options?.text,
+        status: 'В РАБОТЕ',
+      });
+      await writeJsonData(todoPath, taskList);
+      log(chalk.greenBright('Задача добавлена с идентефикатором'), id);
+    } else {
+      log(chalk.magenta('Задача не может быть добавлена, нет описания задачи'));
+    }
   }
-
-
-  // todo list
-  // list: вывести список всех задач.
-  if (options.list) {
-    taskList = readJsonData(todoPath); // здесь читаем todolist.json
-    log(`Список задач:`)
-    taskList.forEach((task, index) => {
-      log(`\x1b[33m${index}. \x1b[0m[${task.status}] ${task.title}`);
-    })
-  }
-
 
   // todo update N title
   // update <id:number> <"newTask":string>:
   // обновить задачу с указанным идентификатором.
-  // todo options.update = updatedTask
-  updatedTask = {
-    id: N,
-    title: 'Обновленный title',
-    status: 'тот же'
-  }
+  // options.update = updatedTask
+  // const updatedTask = {
+  //   id: 4, //N,
+  //   title: 'Обновленный title',
+  //   status: 'тот же ejy bhb',
+  // };
   // т.е. обновляем title задачи номер id
   if (options.update) {
-
-    log(`Задача с идентефикатором ${id} обновлена`)
+    // todo validate id
+    const taskList = await readJsonData(todoPath);
+    const id = options.id;
+    taskList[id-1] = {
+      title: options?.text,
+      status: taskList[id-1]?.status,
+    };
+    // todo use Object.assing(objold, objnew)
+    log(`Задача с идентефикатором ${id} обновлена`);
+    await writeJsonData(todoPath, taskList);
   }
 
-  // todo status N 
+  // todo status N
+  // status <id:number> <"newStatus":string>: обновить статус задачи с указанным идентификатором.
   if (options.status) {
-    const id = options.get;
-    const task = getTask(id);
-    log(`Статус задачи с идентефикатором ${task.id} обновлен`)
+    const taskList = await readJsonData(todoPath);
+    const id = options.id;
+    taskList[id-1].status = options?.text;
+    log(`Статус задачи с идентефикатором ${id} обновлен`);
+    await writeJsonData(todoPath, taskList);
   }
-
 
   // todo get
   // get <id:number>: вывести информацию о задаче с указанным идентификатором.
   if (options.get) {
-    const id = options.get;
-    // todo validate id number (id=1..len)
-    const task = taskList[id];
-    log(`задача с идентефикатором ${id}:` +
-        `Название: ${task?.title}` +
-        `Статус: ${task?.status}`);
+    if (options.id) {
+      const taskList = await readJsonData(todoPath);
+      const id = !isNaN(parseInt(options.id)) ? +options.id : 0;
+      const task = taskList[id-1];
+      if(task) {
+        log(
+          `\nЗадача с идентефикатором ${chalk.yellowBright(id)}:\n` +
+          `Название: ${task?.title}\n` +
+          `Статус: ${task?.status}\n`,
+        );
+      } else {
+        log(chalk.redBright('Такая задача отсутствует'));
+      }
+    } else {
+      log(chalk.magenta('Задача не может быть получена, отсутствует id'));
+    }
   }
 
 
   // todo delete
-  if (options.delete || options.del) {
-    taskList.unshift()
-    writeTasks(taskList)
-    log(`Задача с идентефикатором ${task.id} удалена`)
+  // delete <id:number>: удалить задачу с указанным идентификатором.
+  if (options.delete) {
+    if (options.id) {
+      const taskList = await readJsonData(todoPath);
+      const id = options.id;
+      
+      if (taskList[id-1]) {
+        taskList.splice(id-1, 1); // удаляем элемент массива
+        await writeJsonData(todoPath, taskList); // перезаписываем в файл
+        log(chalk.greenBright(`Задача с идентефикатором ${id} удалена`));
+      } else {
+        log(chalk.redBright('Такая задача отсутствует'));
+      }
+
+    } else {
+      log(chalk.magentaBright('Задача НЕ может быть удалена, отсутствует id'));
+    }
   }
-
-
 };
 
 app();
