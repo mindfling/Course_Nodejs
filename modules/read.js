c05b10a675d75292385ac91e76067452578adb6e
Nepochtov Dmitry <dimendroider@gmail.com>
2024-12-28 09:01:20 +0300

    path test enter

--- modules/read.js
+++ modules/read.js
@@ -2,12 +2,9 @@ import fs, { readFile } from 'node:fs/promises';
 
 // Чтение данных из файла
 // Возвращает сырой буффер данных
-// ? нужно ли проверять пути и аргументы методов
-// todo path check
 export const read = async path => {
   try {
     const data = await readFile(path);
-    console.log(`Файл "${path}" успешно прочитан`);
     return data;
   } catch (err) {
     console.error(`Ошибка чтения файла "${path}" : ${err.message}`);
@@ -22,10 +19,6 @@ export const readJsonData = async (path) => {
   return await read(path) // читаем файл в буфер
     .then(buff => buff.toString('utf8')) // перекодируем буфер в строку
     .then(text => JSON.parse(text)) // парсим текстовую строку в json
-    .then(json => {
-      console.log(`Файл "${path}" успешно прочитан и распарсин`);
-      return json; // возвращаем объект json
-    })
     .catch(err => {
       console.error(`При чтении и парсинге произошла Ошибка" : ${err.message}`);
     });
