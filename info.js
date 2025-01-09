4f2b9dceee3783ca936a8c2427cc5ac70338804d
Nepochtov Dmitry <dimendroider@gmail.com>
2025-01-09 21:02:25 +0300

    Feat it should be work

--- info.js
+++ info.js
@@ -2,8 +2,11 @@ import { log } from 'node:console';
 import * as os from 'node:os';
 import { Buffer } from 'node:buffer';
 
-
+// console colors
 const c = {
+  black(str) {
+    return `\x1b[30m${str}\x1b[0m`;
+  },
   red(str) {
     return `\x1b[31m${str}\x1b[0m`;
   },
@@ -49,28 +52,28 @@ const info = () => {
   );
 }
 
+
 // * ПРОЦЕССОР
 const infoCPU = (arg) => {
   const CPUS = os.cpus();
-  const CPUFirst = CPUS[0]; //todo it is only FirstCPU
-
-
+  const CPUFirst = CPUS[0];
+  const CPUFirstModel = CPUFirst.model;
+  const CPUFirstSpeed = CPUFirst.speed;
+  //todo it is only FirstCPU
   const CPUnumber = CPUS.length; // 8
   const parallelism = os.availableParallelism(); // 8
   const CPUspeed = `~${CPUS[0].speed} МГц`; // 3392
   const arch = os.arch();
   const machine = os.machine();
   // log(CPUFirst, CPUspeed, 'x' + CPUnumber + ' шт'); // cpuz 'Intel(R) Core(TM) i7-3770 CPU @ 3.40GHz'
-  
   log(c.blue('Architecture:'), arch); // 
   log(c.blue('Machine:'), machine); // mach x86_64
-  log(c.blue('OS Platform: '), os.platform());
+  log(c.blue('CPU logical:'), c.green(CPUFirst.model), CPUFirst.speed,);
   log(c.blue('CPU Speed:'), CPUspeed);
-  log(c.blue('CPU logical:'), CPUFirst.model, CPUFirst.speed,);
   log(c.blue('CPU numbers:'), CPUnumber);
   log(c.blue('Parallelism:'), parallelism);
-  
 }
+
 
 // * ОПЕРАТИВНАЯ ПАМЯТЬ
 const infoMemory = (arg) => {
@@ -81,40 +84,29 @@ const infoMemory = (arg) => {
   
   log(
     c.blue('Total:\t'),
-    c.cyan((mem / 2 ** 30).toFixed(3)),
-    c.grey('ГБайт'),
-    c.cyan(mem),
-    c.grey('Байт'),
-    c.cyan(mem / 1024),
-    c.grey('кБайт'),
-    c.cyan((mem / (1024 * 1024)).toFixed(2)),
-    c.grey('МБайт'),
+    c.cyan((mem / 2 ** 30).toFixed(3)), c.grey('ГБайт'),
+    c.cyan(mem), c.grey('Байт'),
+    c.cyan(mem / 1024), c.grey('кБайт'),
+    c.cyan((mem / (1024 * 1024)).toFixed(2)), c.grey('МБайт'),
   );
   log(
     c.blue('Free:\t'),
-    c.cyan((free / 2 ** 30).toFixed(3)),
-    c.grey('ГБайт'),
-    c.cyan(mem),
-    c.grey('Байт'),
-    c.cyan(free / 1024),
-    c.grey('кБайт'),
-    c.cyan((free / (1024 * 1024)).toFixed(2)),
-    c.grey('МБайт'),
+    c.cyan((free / 2 ** 30).toFixed(3)), c.grey('ГБайт'),
+    c.cyan(mem), c.grey('Байт'),
+    c.cyan(free / 1024), c.grey('кБайт'),
+    c.cyan((free / (1024 * 1024)).toFixed(2)), c.grey('МБайт'),
   );
   log(
     c.blue('Used:\t'),
-    c.cyan((used / 2 ** 30).toFixed(3)),
-    c.grey('ГБайт'),
-    c.cyan(mem),
-    c.grey('Байт'),
-    c.cyan(used / 1024),
-    c.grey('кБайт'),
-    c.cyan((used / (1024 * 1024)).toFixed(2)),
-    c.grey('МБайт'),
+    c.cyan((used / 2 ** 30).toFixed(3)), c.grey('ГБайт'),
+    c.cyan(mem), c.grey('Байт'),
+    c.cyan(used / 1024), c.grey('кБайт'),
+    c.cyan((used / (1024 * 1024)).toFixed(2)), c.grey('МБайт'),
   );
 
-  console.log(c.blue('memory Usage: '), c.cyan(memoryUsage), '%');
+  log(c.blue('Usage:\t'), c.cyan(memoryUsage), '%');
 }
+
 
 // * ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ
 const infoUser = (arg) => {
@@ -131,6 +123,7 @@ const infoUser = (arg) => {
   log(c.blue('User Shell:'), users.shell);
 }
 
+
 // * СЕТЕВЫЕ ПОДКЛЮЧЕНИЯ
 const infoNetwork = (arg) => {
   const nets = os.networkInterfaces();
@@ -146,7 +139,8 @@ const infoNetwork = (arg) => {
     });
   }
 }
-  
+
+
 // * ИНФОРМАЦИЯ ОБ ОПЕРАЦИОННОЙ СИСТЕМЕ
 const infoSystem = (arg) => {
   const host = os.hostname();
@@ -154,38 +148,32 @@ const infoSystem = (arg) => {
   const release = os.release();
   const platform = os.platform();
   const version = os.version();
-  log(c.blue('OS Hostname:'), host); // ""
+  log(c.blue('OS Hostname:'), host); // "OFFICEPC"
   log(c.blue('OS Type:'), type); // "Windows_NT"
+  log(c.blue('OS Version:'), version); // "Windows 10 Pro"
   log(c.blue('Build Release:'), release); // "10.0.14393"
-  log(c.blue('Build version:'), version); // ""
   log(c.blue('Platform:'), platform); // "win32"
+  log(c.blue('Arch:'), os.arch()); // ""
 }
 
 
 const app = () => {
-
-  // log(os)
-  const info = {};
-
-  log();
-  // log('Host Name: ', info.host);
-  // log('Home Folder: ', info.home);
-  // log('Temp Folder: ', info.temp);
-
+  const info = {}; // Our Info
+  
   log(c.green('\nCPUS:'))
   infoCPU();
 
   log(c.green('\nMemory:'))
   infoMemory();
+  
+  log(c.green('\nOperation System:'))
+  infoSystem();
+  
+  log(c.green('\nUsers:'))
+  infoUser();
 
   log(c.green('\nNetworks:'))
   infoNetwork();
-  
-  log(c.green('Users:'))
-  infoUser();
-  
-  log(c.green('Operation System:'))
-  infoSystem();
 }
 
 
