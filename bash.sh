8b900d2f6f650388d179d62ac7e8cf71bab4cc94
Nepochtov Dmitry <dimendroider@gmail.com>
2025-03-01 20:57:34 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,28 @@
+# my ll aliases
+alias ls='ls -F --color=auto --show-control-chars'
+alias l='ls -lsah  --group-directories-first'
+alias ll='ls -l'
+
+alias a=alias
+a status='git status'
+a branch='git branch'
+a log='git log --all'
+a graph='git log --all --graph'
+a add='git add .'
+
+
+alias a='alias'
+alias add='todo add'
+alias branch='git branch'
+alias get='todo get'
+alias graph='git log --all --graph'
+alias help='todo help'
+alias l='ls -lsah  --group-directories-first'
+alias list='todo list'
+alias ll='ls -l'
+alias log='git log --all'
+alias ls='ls -F --color=auto --show-control-chars'
+alias node='winpty node.exe'
+alias status='todo status'
+alias todo='node todo'
+alias winget='winpty winget.exe'
